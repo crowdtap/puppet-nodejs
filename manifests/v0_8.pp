7d960ec20d1aba23af362f8119cca2c0a8228203
Michael D'Auria <michael.dauria@gmail.com>
2013-05-15 12:17:07 -0400

    Set the default 0.8 to 0.8.23

--- manifests/v0_8.pp
+++ manifests/v0_8.pp
@@ -6,11 +6,11 @@
 
 class nodejs::v0_8 {
   include nodejs::config
-  include nodejs::v0_8_8
+  include nodejs::v0_8_23
 
   file { "${nodejs::config::root}/versions/v0.8":
     ensure => link,
-    target => "${nodejs::config::root}/versions/v0.8.8"
+    target => "${nodejs::config::root}/versions/v0.8.23"
   }
 }
 
